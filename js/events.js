7a73830f3c7110d0af0dbd56c11e7a73490f8fca
Rwanda S <rwanda.ace@gmail.com>
2020-02-17 20:32:51 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,33 @@
 //define functions here
 
+function getIt(){
+$('p').on('click', function(){
+  alert("Hey!")
+})  
+}
+function frameIt(){
+$(document).on('load', function(){
+  $('img')[0].className = 'tasty'
+ 
+}
+)}
+function pressIt(){
+  $(document).on('keydown', function(key){
+    if(key.which == 71){
+      alert('You have pressed the G key');
+    }
+  })
+}
+
+function submitIt(){
+  $("form").on("submit", function(){
+    alert('Your form is going to be submitted now.')
+  })
+}
+
 $(document).ready(function(){
 
 // call functions here
-
+pressIt()
+frameIt()
 });
